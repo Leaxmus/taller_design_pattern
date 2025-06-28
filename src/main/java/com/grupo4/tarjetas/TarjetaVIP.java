f05ee4f5a891d866df54d97c07f3d5155104de2b
Bryan Zhang <140031745+bgzhangg@users.noreply.github.com>
2025-06-27 23:29:10 -0500

    Create TarjetaVIP.java

---
+++ src/main/java/com/grupo4/tarjetas/TarjetaVIP.java
@@ -0,0 +1,14 @@
+
+
+
+public class TarjetaVIP extends Tarjeta {
+
+    public TarjetaVIP() {
+        super(1000, 0, 50000.0);
+    }
+
+    @Override
+    public boolean pago(double monto) {
+        //Implementacion
+    }
+}
