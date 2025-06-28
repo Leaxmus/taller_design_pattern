f4754a71b1e19614997d99e6250d88a2a2f37c1c
Derian Baque <dfbaque@espol.edu.ec>
2025-06-27 21:34:35 -0500

    "Implementación sistema de notificaciones con Decorator y Adapter"

--- src/main/java/com/grupo4/Main.java
+++ src/main/java/com/grupo4/Main.java
@@ -1,4 +1,5 @@
 package com.grupo4;
+import com.grupo4.notificaciones.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,5 +7,14 @@ public class Main {
         Facade fachadaAtencion = new Facade();
         fachadaAtencion.atenderVirtual("movil");
         //fachadaAtencion.atenderPrensencial();
+    
+    // Parte tuya: notificación simple con Decorator y Adapter
+        INotificacion notificacion = new NotificacionSMS();               // Notificación base (obligatoria)
+        notificacion = new NotificacionEmail(notificacion);               // Agrega Email (obligatorio)
+        notificacion = new NotificacionWhatsApp(notificacion);            // Agrega WhatsApp (opcional)
+        notificacion = new SignalAdapter(notificacion);                   // Agrega Signal (adaptador)
+
+        notificacion.enviar("Recuerde pagar su tarjeta antes del 5 de julio.");
+    
     }
 }
