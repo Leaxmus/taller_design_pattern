838176996a72fbf9923c00773cc899738298fedb
Bryan Zhang <140031745+bgzhangg@users.noreply.github.com>
2025-06-27 23:28:47 -0500

    Create TarjetaPremium.java

---
+++ src/main/java/com/grupo4/tarjetas/TarjetaPremium.java
@@ -0,0 +1,14 @@
+
+
+
+public class TarjetaPremium extends Tarjeta {
+
+    public TarjetaPremium() {
+        super(500, 2, 20000.0); 
+    }
+
+    @Override
+    public boolean pago(double monto) {
+        //Implementacion
+    }
+}
