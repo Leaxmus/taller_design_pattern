0d67f6cbc9d4c359ef10b3928e9f333e80db6888
Bryan Zhang <140031745+bgzhangg@users.noreply.github.com>
2025-06-27 23:28:17 -0500

    Create TarjetaBasica.java

---
+++ src/main/java/com/grupo4/tarjetas/TarjetaBasica.java
@@ -0,0 +1,13 @@
+
+
+public class TarjetaBasica extends Tarjeta {
+
+    public TarjetaBasica() {
+        super(100, 5, 5000.0);
+    }
+
+    @Override
+    public boolean pago(double monto) {
+        ///Implementacion
+    }
+}
